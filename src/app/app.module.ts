757a8dba6fa30f8a0158363d52f1854208f0ed58
sfountaine <syfountaine@gmail.com>
2023-04-19 09:27:23 -0700

    Initial commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,5 +1,11 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
+
+import { FontAwesomeModule, FaIconLibrary } from '@fortawesome/angular-fontawesome';
+import { faCircle,faSquare } from '@fortawesome/free-solid-svg-icons';
+import { faCircle as farCircle,faSquare as farSquare } from '@fortawesome/free-regular-svg-icons';
+import { faStackOverflow, faGithub, faMedium } from '@fortawesome/free-brands-svg-icons';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
@@ -10,9 +16,18 @@ import { AppComponent } from './app.component';
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    FontAwesomeModule,
+    NgbModule
   ],
   providers: [],
   bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule {
+  constructor(library: FaIconLibrary) {
+
+    // Add an icon to the library for convenient access in other components
+    library.addIcons(faCircle,faSquare,farCircle,farSquare
+          ,faStackOverflow,faGithub,faMedium);
+  }
+}
